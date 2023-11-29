d31c5289d836e0a89b476ee98eefd2400c5c29cb
wnals9 <wnals16@naver.com>
2023-11-29 10:50:30 +0900

    주민커밋

--- drawauction/src/main/java/com/gdu/drawauction/service/DrawServiceImpl.java
+++ drawauction/src/main/java/com/gdu/drawauction/service/DrawServiceImpl.java
@@ -1,5 +1,13 @@
 package com.gdu.drawauction.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
 public class DrawServiceImpl implements DrawService{
 
 }
