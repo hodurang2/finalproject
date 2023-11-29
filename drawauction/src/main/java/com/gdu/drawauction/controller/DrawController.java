2afcf06892ebac6c71018937db8675453afc8e3a
wnals9 <wnals16@naver.com>
2023-11-29 14:31:38 +0900

    주민커밋

--- drawauction/src/main/java/com/gdu/drawauction/controller/DrawController.java
+++ drawauction/src/main/java/com/gdu/drawauction/controller/DrawController.java
@@ -6,8 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.gdu.drawauction.service.DrawService;
 
@@ -24,11 +26,24 @@ public class DrawController {
   public String list() {
     return "draw/list";
   }
+  
+  @GetMapping("/write.form")
+  public String write() {
+    return "draw/write";
+  }
 	
   @ResponseBody
   @GetMapping(value="/getList.do", produces="application/json")
   public Map<String, Object> getList(HttpServletRequest request){
     return drawService.getDrawList(request);
   }
+  /*
+  @ResponseBody
+  @PostMapping(value="/imageUpload.do", produces="application/json")
+  public Map<String, Object> imageUpload(MultipartHttpServletRequest multipartRequest) {
+    return drawService.imageUpload(multipartRequest);
+  }
+  */
+  
 
 }
--- drawauction/src/main/java/com/gdu/drawauction/service/DrawService.java
+++ drawauction/src/main/java/com/gdu/drawauction/service/DrawService.java
@@ -4,7 +4,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
 public interface DrawService {
 	public Map<String, Object> getDrawList(HttpServletRequest request);
+	//public Map<String, Object> imageUpload(MultipartHttpServletRequest multipartRequest);
 
 }
