b8f6a6cb08e1b6d4dcb046859389d6be9117b7d7
unknown <whtjqls0504@naver.com>
2023-11-29 10:33:54 +0900

    231129-seobb

--- drawauction/src/main/java/com/gdu/drawauction/dto/UserDto.java
+++ drawauction/src/main/java/com/gdu/drawauction/dto/UserDto.java
@@ -12,18 +12,21 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UserDto {
-  private int userNo;
-  private String email;
-  private String pw;
-  private String name;
-  private String gender;
-  private String mobile;
-  private String postcode;
-  private String roadAddress;
-  private String jibunAddress;
-  private String detailAddress;
-  private int agree;
-  private int state;
-  private Date pwModifiedAt ;
-  private Date joinedAt ;
+	
+	private int userNo;
+	private String email;
+	private String pw;
+	private String name;
+	private String mobile;
+	private int agree;
+	private int state;
+	private Date joinedAt;
+	private String postcode;
+	private String roadAddress;
+	private String jibunAddress;
+	private String detailAddress;
+	private String nickname;
+	private String introduction;
+	
+	
 }
