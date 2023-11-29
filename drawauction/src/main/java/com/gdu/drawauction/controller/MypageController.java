5eedb5099c2f5518da09d63e12a3d6f6b5b95477
kdh23232 <kdh23232@naver.com>
2023-11-29 20:09:29 +0900

    다혜커밋

---
+++ drawauction/src/main/java/com/gdu/drawauction/controller/MypageController.java
@@ -0,0 +1,13 @@
+package com.gdu.drawauction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/mypage")
+@RequiredArgsConstructor
+@Controller
+public class MypageController {
+
+}
