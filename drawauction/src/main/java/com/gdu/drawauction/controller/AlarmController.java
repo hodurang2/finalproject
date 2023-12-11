d2792c965da1cd090a9fe5e48a5195f0c49fcc7f
wnals9 <wnals16@naver.com>
2023-12-11 17:29:13 +0900

    주민커밋

---
+++ drawauction/src/main/java/com/gdu/drawauction/controller/AlarmController.java
@@ -0,0 +1,35 @@
+package com.gdu.drawauction.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdu.drawauction.dto.AlarmDto;
+import com.gdu.drawauction.service.AlarmService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class AlarmController {
+	
+	private final AlarmService alarmService;
+    
+	@ResponseBody
+	@GetMapping(value="/alarm/getAlarmList.do", produces = "application/json")
+	public List<AlarmDto> getAlarmList(HttpServletRequest request) {
+	  return alarmService.getAlarmList(request);
+	}
+	
+	@ResponseBody
+	@GetMapping(value="/alarm/alarmCheck.do", produces = "application/json")
+	public int alarmCheck(HttpServletRequest request, Model model)  {
+	  int checkResult = alarmService.alarmCheck(request);
+	  return checkResult;
+	}
+}
---
+++ drawauction/src/main/java/com/gdu/drawauction/service/AlarmServiceImpl.java
@@ -0,0 +1,35 @@
+package com.gdu.drawauction.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gdu.drawauction.dao.AlarmMapper;
+import com.gdu.drawauction.dto.AlarmDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class AlarmServiceImpl implements AlarmService  {
+	
+	private final AlarmMapper alarmMapper;
+
+	@Override
+	public List<AlarmDto> getAlarmList(HttpServletRequest request)  {
+	  String email = request.getParameter("email");
+	  List<AlarmDto> alarmList = alarmMapper.getAlarmList(email);
+	  
+	  return alarmList;
+	}
+	
+	@Override
+	public int alarmCheck(HttpServletRequest request) {
+		String email = request.getParameter("email");
+		return alarmMapper.alarmCheck(email);
+	}
+}
